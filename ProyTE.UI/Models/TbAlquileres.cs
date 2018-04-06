0932ff4e10d53785f88f43f69d9ac977f0ef7aed
Daqd1491 <diegoqd1491@gmail.com>
2018-04-05 20:10:22 -0600

    DataAnnotations de Alquileres

--- ProyTE.UI/Models/TbAlquileres.cs
+++ ProyTE.UI/Models/TbAlquileres.cs
@@ -13,20 +13,30 @@ namespace ProyTE.UI.Models
 
         public int Id_Usuario { get; set; }
 
+        [Required(ErrorMessage = "Estado requerido")]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
         public string Estado { get; set; }
 
+        [Required(ErrorMessage = "Entrega requerida")]
         public DateTime Entrega { get; set; }
 
+        [Required(ErrorMessage = "Devolucion requerida")]
         public DateTime Devuelve { get; set; }
 
+        [Required(ErrorMessage = "Precio requerido")]
+        [Range(5000, 500000, ErrorMessage = "El Precio debe ser entre 5000 - 500000")]
         public decimal Precio { get; set; }
 
+        [Range(5000, 500000, ErrorMessage = "La multa debe ser entre 5000 - 500000")]
         public decimal? Multa { get; set; }
 
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
         public string Reparacion { get; set; }
 
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
         public string Detalle { get; set; }
 
+        [Required(ErrorMessage = "Fecha de alquiler requerida")]
         public DateTime FechaAlquiler { get; set; }
     }
 }
