2bd15c6faa9d8e0737b207d2f4593bd13e36d184
rvaa6 <ravaa6@gmail.com>
2018-04-07 12:41:36 -0600

    Update TbTarjetas.cs

--- ProyTE.UI/Models/TbTarjetas.cs
+++ ProyTE.UI/Models/TbTarjetas.cs
@@ -22,6 +22,7 @@ namespace ProyTE.UI.Models
         public decimal NumTarjeta { get; set; }
 
         [Required(ErrorMessage = "Fecha de vencimiento requerida")]
+        //Datetime picker
         public DateTime FechaVencimiento { get; set; }
     }
 }
